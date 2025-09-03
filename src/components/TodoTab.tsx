caffa9bc0d77a2de10536a7ac8ad666ea8548343
newseob <newseobdesign@gmail.com>
2025-09-04 01:52:54 +0900

    식단 기입 추가

--- src/components/TodoTab.tsx
+++ src/components/TodoTab.tsx
@@ -9,15 +9,15 @@ export default function TodoTab() {
       <div className="mx-auto w-full max-w-screen-2xl">
         <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6 justify-center">
           {/* 각 카드(열) 최대폭 제한 */}
-          <section className="w-full max-w-[560px]">
+          <section className="w-full">
             <TodayRoutine />
           </section>
 
-          <section className="w-full max-w-[560px]">
+          <section className="w-full">
             <TodoBoxSection />
           </section>
 
-          <section className="w-full max-w-[560px]">
+          <section className="w-full">
             <HaruRecord />
           </section>
         </div>
