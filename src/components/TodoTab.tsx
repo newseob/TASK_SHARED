364ad11d962acb7098e666f7269ae4534526a7f0
newseob <newseobdesign@gmail.com>
2025-09-04 00:59:32 +0900

    식단 기입 추가

--- src/components/TodoTab.tsx
+++ src/components/TodoTab.tsx
@@ -1,5 +1,4 @@
 // TodoTab.tsx
-
 import TodayRoutine from "./Todo/TodayRoutine.tsx";
 import TodoBoxSection from "./Todo/TodoBoxSection.tsx";
 import HaruRecord from "./Todo/HaruRecord.tsx";
@@ -8,8 +7,8 @@ export default function TodoTab() {
   return (
     <div className="bg-white text-black dark:bg-zinc-900 dark:text-white">
       <div className="mx-auto w-full max-w-screen-2xl">
-        <div className="grid grid-cols-1 lg:[grid-template-columns:repeat(3,max-content)] gap-6 justify-center">
-          {/* 각 카드(열) 최대폭 제한: 필요 시 숫자 조절 (예: 560px) */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6 justify-center">
+          {/* 각 카드(열) 최대폭 제한 */}
           <section className="w-full max-w-[560px]">
             <TodayRoutine />
           </section>
