2cf98eed728fe12d21d8dd38dbab27c481751b26
newseob <c0olluck@naver.com>
2026-03-18 01:39:52 +0900

    레이아웃 수정

--- src/components/TodoTab.tsx
+++ src/components/TodoTab.tsx
@@ -8,37 +8,47 @@ import DateReference from "./Todo/DateReference.tsx";
 
 export default function TodoTab() {
   return (
-    <div className="bg-gray-200 text-black dark:bg-black dark:text-white overflow-x-hidden select-none">
-      <div className="mx-auto w-full max-w-screen-lg px-0 py-4 grid gap-2 grid-cols-1 xs:grid-cols-2">
-
-
-        {/* 루틴 */}
-        <section className="w-full">
-          <div className="bg-white dark:bg-zinc-900 rounded-lg p-1">
-            <div className="grid gap-3">
-              <TodayRoutine />
-              <DateReference />
-              <CycleRoutine />
-              <LinkBox />
-            </div>
-          </div>
-        </section>
+    <div className="bg-gray-200 text-black dark:bg-black dark:text-white oveflow-x-hidden select-none">
+      <div className="mx-auto w-full max-w-screen-lg px-1 flex flex-col gap-16 py-4">
 
         {/* Todo */}
         <section className="w-full">
           <div className="bg-white dark:bg-zinc-900 rounded-lg p-1">
-            <div className="grid gap-3">
-              <TodoBoxSection />
+            <TodoBoxSection />
+          </div>
+        </section>
+
+        {/* 루틴 */}
+        <section className="w-full">
+          <div className="bg-white dark:bg-zinc-900 rounded-lg p-1">
+            <div className="grid grid-cols-1 xs:grid-cols-2 lg:grid-cols-2 gap-3">
+              <TodayRoutine />
+              <CycleRoutine />
             </div>
           </div>
         </section>
 
+        {/* 날짜참고 */}
+        <section className="w-full">
+          <div className="bg-white dark:bg-zinc-900 rounded-lg p-1">
+            <DateReference />
+          </div>
+        </section>
+
         {/* Money */}
-        <section className="w-full col-span-2">
+        <section className="w-full">
           <div className="bg-white dark:bg-zinc-900 rounded-lg p-1">
             <MoneyBox />
           </div>
         </section>
+
+        {/* 링크 */}
+        <section className="w-full">
+          <div className="bg-white dark:bg-zinc-900 rounded-lg p-1">
+            <LinkBox />
+          </div>
+        </section>
+
       </div>
     </div>
   );
